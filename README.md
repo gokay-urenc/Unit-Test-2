179af46c9974947c9965daecaf2d6b2d351a4249
Gökay <48670495+gokay-urenc@users.noreply.github.com>
2020-05-10 17:47:50 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Unit-Test-2
+ Birim testi. / Unit test.
---
+++ ShoppingCart.Tests/CartTests.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ShoppingCart.Tests
+{
+    [TestClass]
+    public class CartTests
+    {
+        private CartItem _cartItem;
+        private CartManager _cartManager;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _cartManager = new CartManager();
+            _cartItem = new CartItem
+            {
+                Product = new Product()
+                {
+                    ProductId = 1,
+                    ProductName = "Laptop",
+                    UnitPrice = 2500
+                },
+                Quantity = 1
+            };
+            _cartManager.Add(_cartItem);
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            _cartManager.Clear();
+        }
+
+        [TestMethod]
+        public void Products_must_be_added_to_the_cart()
+        {
+            // Arrange
+            const int expected = 1;
+
+            // Act
+            var totalNumberOfCartItems = _cartManager.TotalItems;
+
+            // Assert
+            Assert.AreEqual(expected, totalNumberOfCartItems);
+        }
+
+        [TestMethod]
+        public void Products_must_be_removed_from_the_cart()
+        {
+            // Arrange
+            var totalNumberOfCartItems = _cartManager.TotalItems;
+
+            // Act
+            _cartManager.Remove(1);
+            var totalNumberOfCartItemsLeft = _cartManager.TotalItems;
+
+            // Assert
+            Assert.AreEqual(totalNumberOfCartItems - 1, totalNumberOfCartItemsLeft);
+        }
+
+        [TestMethod]
+        public void The_card_must_be_cleared()
+        {
+            // Arrange
+
+            // Act
+            _cartManager.Clear();
+
+            // Assert
+            Assert.AreEqual(0, _cartManager.TotalQuantity);
+            Assert.AreEqual(0, _cartManager.TotalItems);
+        }
+    }
+}
